f6db4c7feb2da89d75c7d4c0e0678822729c5328
pknb213 <pknb213>
2018-08-27 18:11:33 +0900

    08.27 Update

--- Microservice Architecture/Distributor/client.js
+++ Microservice Architecture/Distributor/client.js
@@ -3,10 +3,51 @@
 const net = require('net');
 
 class topClient{
-  topClient(){
-    console.log("생성자입니다");
+  constructor(host, port, onCreate, onRead, onEnd, onError){
+      this.options = {
+        host: host,
+        port: port
+      };
+      this.onCreate = onCreate;
+      this.onRead = onRead;
+      this.onEnd = onEnd;
+      this.onError = onError;
+  }
+
+  connect(){                                              // 접속 처리 함수
+    this.client = net.connect(this.options,() => {
+      if(this.onCreate)
+      this.onCreate(this.options);                        // 접속 완료 이벤트 콜백
+    });
+    this.client.on('data', (data) =>{                     // 데이터 수신 처리
+      var sz = this.merge? this.merge + data.toString() : data.toString();
+      var arr = sz.split('/n');
+      for(var n in arr){
+        if(sz.charAt(sz.length -1) != 'q' && n ==arr.length - 1){
+          this.merge = arr[n];
+          break
+        }else if(arr[n] == ""){
+          break;
+        }else{
+          this.onRead(this.options, JSON.parse(arr[n]));
+        }
+      }
+    });
+
+    this.client.on('close',() =>{                          // 접속 종료 처리
+      if(this.onEnd)
+        this.onEnd(this.options);
+    });
+    this.client.on('error', (err) =>{                      // 에러 발생 처
+      if(this.onError)
+        this.onError(this.options,err);
+    });
+
+    write(packet){
+      this.client.write(JSON.stringify(packet) + '/n');
+    }
   }
 }
 
-var c1 = new topClient;
-c1.topClient();
+
+module.export = topClient;
---
+++ Microservice Architecture/Distributor/server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const net = require('net');
+const tcpClient = require('./client.js');     // Client class 참조
+
+class tcpServer{                              // class 선언
+
+}
+
+module.exports = tcpServer;                  // exports 선언
